cf1f2f34b01294986ba4570d4f23bfed3fe8a493
FariqJavier <--global>
2024-06-05 09:01:51 +0700

    feat: add mysql12

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,11 @@
 import { Sequelize } from 'sequelize';
 import logger from "./logger";
+import mysql2 from 'mysql2'; 
 
 const sequelize = new Sequelize('demiwatch', 'admin', 'admin', {
   host: 'localhost',
   dialect: 'mysql',
+  dialectModule: mysql2,
   logging: (sql, timing) => {
     // Your custom logging logic here
     logger.warn(sql); // Log the generated SQL query
