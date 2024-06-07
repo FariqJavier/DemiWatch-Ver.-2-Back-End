7c3269d9017faad559c57ac592dd16de70889f83
FariqJavier <--global>
2024-06-07 15:02:02 +0700

    feat: fix config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD!, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  port: 3306,
   dialectModule: mysql2,
   logging: (sql, timing) => {
     // Your custom logging logic here
